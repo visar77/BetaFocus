cd1792a6d1017584fbe9d081b2719441985f28f9
visar77 <visarlumi77@gmail.com>
2024-05-21 11:41:19 +0200

    Added comments + start_session() now works without setting a time

--- BetaFocus/src/api/microcontroller.py
+++ BetaFocus/src/api/microcontroller.py
@@ -1,4 +1,6 @@
+import glob
 import os
+import sys
 import time
 from os.path import dirname as up_dir
 from datetime import datetime, timezone
@@ -11,20 +13,38 @@ class MicroController:
     Represents the MicroController.
     """
 
-    def __init__(self, port, baudrate=9600, timeout=3.0):
+    def __init__(self, port, baudrate=9600, timeout=10.0):
+        """
+        Creates serial connection with serial library.
+        :param port: serial port
+        :param baudrate: serial baudrate (default: 9600)
+        :param timeout: seconds after a timeout sets in (timeout: 10 seconds)
+        """
         self.__port = port
         self.__baudrate = baudrate
-        self.__ser = serial.Serial(port, baudrate, timeout=3.0)
+        self.__ser = serial.Serial(port, baudrate, timeout=timeout)
         self.__timeout = timeout
 
     def is_open(self):
+        """
+        Checks if serial connection is open
+        :return: boolean, true or false
+        """
         return self.__ser.is_open
 
     def open(self):
+        """
+        Opens serial connection and flushes input
+        :return:
+        """
         self.__ser.open()
         self.__ser.flushInput()
 
     def last_package(self):
+        """
+        Reads the last package from the serial connection.
+        :return: string, the last package in CSV format OR None if no package was found
+        """
         while True:
             line = str(self.__ser.readline())
             if line == "b''" or line == '':  # This means readLine went to timeout
@@ -36,9 +56,17 @@ class MicroController:
         return None
 
     def close(self):
+        """
+        Closes the serial connection
+        :return:
+        """
         self.__ser.close()
 
     def check_connection(self):
+        """
+        Checks if the connection is still available
+        :return:
+        """
         ports = self.get_available_ports()
         for p in ports:
             if self.__port in p:
@@ -46,37 +74,71 @@ class MicroController:
         return False
 
     def switch_connection(self, port, baudrate=9600):
+        """
+        Switches the connection to another port
+        :param port: serial port
+        :param baudrate: new baudrate (default: 9600)
+        :return:
+        """
         self.__port = port
         self.__baudrate = baudrate
         self.__ser = serial.Serial(port, baudrate)
 
     @staticmethod
     def get_available_ports():
-        return sorted(serial.tools.list_ports.comports())
+        """
+        Returns a list of available ports
+        :raises EnvironmentError: Unsupported or unknown platform
+        :return: list of available ports
+        """
+        ports = [str(x) for x in sorted(serial.tools.list_ports.comports())]
+
+        cut_ports = []
+        for p in ports:
+            if sys.platform.startswith('win'):
+                p = p.split(" - ")[0]
+            elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+                p = p.split(" ")[0]
+            elif sys.platform.startswith("darwin"):
+                p = p.split(" ")[0]
+            else:
+                raise EnvironmentError('Unsupported platform')
+            cut_ports.append(p)
+
+        return cut_ports
 
 
 class MCConnector:
+    """
+    Represents the MicroController Connector. This class is used to start and stop a session. It also handles the
+    pausing and resuming of a session. Sessions are saved in the data folder with the format
+    session_yy-mm-dd_hh-mm-ss-fff.csv.
+    """
 
     def __init__(self, port, baudrate=9600):
+        """
+        Creates a MicroController Connector.
+        :param port: serial port
+        :param baudrate: baudrate (default: 9600)
+        """
         self.__arduino = MicroController(port, baudrate)
-        self.open = True
+        self.open = self.__arduino.is_open()
         self.__packages = []
         self.__pause = False
         self.timer_start = None
         self.paused_time = 0
-        self.timer_time = None
+        self.session_date = None
 
-    def start_session(self, time_set=6.0):
-        date = datetime.now().strftime("%y-%m-%d_%H-%M-%S-%f")
-        # For platform independence
-        # Gives path of data folder
-        path = os.path.join(up_dir(up_dir(up_dir(os.path.realpath(__file__)))), "data")
-        file_name = rf"session_{date}.csv"
-        path = os.path.join(path, file_name)
+    def start_session(self):
+        """
+        Starts a session and logs the data in a list.
+        :return:
+        """
+        self.session_date = datetime.now().strftime("%y-%m-%d_%H-%M-%S-%f")
 
-        self.timer_time = time_set
         self.timer_start = time.monotonic()
         self.open = self.__arduino.is_open()
+
         if not self.open:
             print("Connection not open! Session can't be started")
 
@@ -85,7 +147,6 @@ class MCConnector:
 
             if self.__pause:
                 print("Pausing")
-                package = ";;;;;;;;;;;;;"
                 continue
 
             if package is None:
@@ -96,33 +157,38 @@ class MCConnector:
             print("package received: ", package)
             self.__packages.append(package)
 
-            if self.paused_time + time.monotonic() - self.timer_start >= time_set:
-                self.open = False
-                print("Done with session!")
-                self.stop_session(path, file_name)
+        print("Session stopped")
 
-    def stop_session(self, path: str, file_name: str = None):
-        print(path)
+    def stop_session(self):
+        """
+        Stops the session and writes the data to a csv file.
+        :return:
+        """
+        self.close()  # Close the connection and stops the session logging done by start_session
+
+        path = os.path.join(up_dir(up_dir(up_dir(os.path.realpath(__file__)))), "data")
+        file_name = rf"session_{self.session_date}.csv"
+        path = os.path.join(path, file_name)
+
         # Write a session csv
+        header_session_line = "TIMERTIME;TIMESTAMP;POOR_SIGNAL_QUALITY;ATTENTION;MEDITATION;DELTA;THETA;LOW ALPHA;HIGH ALPHA;LOW BETA;HIGH BETA;LOW GAMMA; MID GAMMA;RAW WAVE DATA\n"
         with open(path, "w+") as f:
-            f.write(
-                "TIMERTIME;TIMESTAMP;POOR_SIGNAL_QUALITY;ATTENTION;MEDITATION;DELTA;THETA;LOW ALPHA;HIGH ALPHA;LOW BETA;HIGH BETA;LOW GAMMA; MID GAMMA;RAW WAVE DATA\n")
+            f.write(header_session_line)
             for pack in self.__packages:
                 f.write(pack + "\n")
 
         session_csv_path = os.path.join(os.path.dirname(path), "sessions.csv")
 
         # Add to sessions.csv or create session.csv
+        header_line = "INDEX;DATE;FILE_NAME;TIMESTAMP_OF_FIRST_VALID_PACKAGE;TIMESTAMP_OF_LAST_VALID_PACKAGE\n"
         if not os.path.exists(session_csv_path):
             with open("../../data/sessions.csv", "a+") as f:
-                f.write(
-                    "INDEX;DATE;FILE_NAME;TIME_SET;TIMESTAMP_OF_FIRST_VALID_PACKAGE;TIMESTAMP_OF_LAST_VALID_PACKAGE\n")
+                f.write(header_line)
 
         with open(session_csv_path, "w+") as f:
             lines = f.readlines()
             if not lines:  # lines is empty
-                f.write(
-                    "INDEX;DATE;FILE_NAME;TIME_SET;TIMESTAMP_OF_FIRST_VALID_PACKAGE;TIMESTAMP_OF_LAST_VALID_PACKAGE\n")
+                f.write(header_line)
                 index = 0
             else:
                 index = lines[-1].split(";")[0]
@@ -141,26 +207,34 @@ class MCConnector:
             while self.__packages[i].split(";")[1] != "" and i > 0:
                 i -= 1
             date_of_last_package = self.__packages[i].split(";")[1]
-            f.write(f"{index};{date};{file_name};{self.timer_time};{date_of_first_package};{date_of_last_package}\n")
+            f.write(f"{index};{date};{file_name};{date_of_first_package};{date_of_last_package}\n")
 
         self.__packages = []
-        self.timer_time = None
+        self.session_date = None
         self.timer_start = None
-        self.close()
 
     def pause_session(self):
+        """
+        Pauses the session.
+        :return:
+        """
         print("PAUSED")
         self.__pause = True
         self.paused_time += time.monotonic() - self.timer_start
 
     def resume_session(self):
+        """
+        Resumes the session.
+        :return:
+        """
         print("RESUMED")
         self.__pause = False
         self.timer_start = time.monotonic()
 
     def close(self):
+        """
+        Closes the connection.
+        :return:
+        """
         self.__arduino.close()
-
-
-ac = MCConnector('COM11')
-ac.start_session(10)
+        self.open = False
