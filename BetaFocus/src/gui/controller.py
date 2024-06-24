59a63a0af4361b29cf30761b810de1b438156b8f
Nane <nane-goette@web.de>
2024-06-24 14:11:15 +0200

    moved timer class into controller module

--- BetaFocus/src/gui/controller.py
+++ BetaFocus/src/gui/controller.py
@@ -1,11 +1,12 @@
+import time
+
 from threading import Thread
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox, QLabel
 
 from api.microcontroller import *
 from .data import Archive, Session
-from .timer import Timer
 from .view import MainWindow
 
 
@@ -124,3 +125,48 @@ class Controller:
         combo_box = self.connect_dialog.combo_box
         combo_box.clear()
         combo_box.addItems(MicroController.get_available_ports())
+
+
+class Timer:
+
+    def __init__(self, time_label: QLabel):
+        self.running: bool = False
+        self.started: bool = False
+        self.stopped: bool = False
+        self.passed: float = 0
+        self.time_label = time_label
+
+    def start(self):
+        self.running = True
+        self.started = True
+        self.stopped = False
+
+    def pause(self):
+        self.running = False
+
+    def resume(self):
+        self.running = True
+
+    def stop(self):
+        self.pause()
+        self.stopped = True
+        self.started = False
+        self.passed = 0
+
+    def count(self):
+        while not self.stopped:
+            start = time.monotonic()
+            if self.started:
+                until_now: float = self.passed
+            else:
+                until_now: float = 0
+            while self.running:
+                self.passed = time.monotonic() - start + until_now
+                self.time_label.setText(self.format_time_string())
+        self.passed = 0
+
+    def format_time_string(self) -> str:
+        secs: float = self.passed % 60
+        mins: float = self.passed // 60
+        hours: float = mins // 60
+        return f"{int(hours):02d}:{int(mins):02d}:{int(secs):02d}"
